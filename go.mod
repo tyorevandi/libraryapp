8c80526c03b736738df9475f54184cf5cebcda9e
tyorevandi <tyorevandi79@gmail.com>
2024-01-01 22:12:26 +0700

    hari pertama

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tyorevandi/libraryapp
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+	var stringText string
+	var intNumber int
+	stringText = "Hello World"
+	intNumber = 1
+	floatNumber := 1.5
+	fmt.Println(stringText,intNumber, floatNumber)
+}
